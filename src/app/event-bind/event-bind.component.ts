b5d2b2ac64c46eeb3ccf87ebfc4378daf5f996d8
vikas576 <vikassharma576@gmail.com>
2019-09-09 11:04:29 +0530

    event binding

--- src/app/event-bind/event-bind.component.ts
+++ src/app/event-bind/event-bind.component.ts
@@ -6,33 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-bind.component.css']
 })
 export class EventBindComponent implements OnInit {
-  public serverStatus="server is not connected.Press click button to connect."
-  public textDanger ="text-danger";
-  public allowButton =false;
-  public textBg ="bg_primary";
-  public triggerType;
-  
-  
-  constructor() { 
+
+  constructor() {
 setTimeout(() => {
-    this.allowButton =true;
+    this.allowButton = true;
 }, 1000);
 
   }
-  onCreateServer(event){
-    this.serverStatus="connected";
-    this.textDanger="text-success";
-    this.textBg="bg_success";
-    console.log(event);
-    this.triggerType= "event trigger type is : " + event.type;
-    
-  }
-  
+  public serverStatus = 'server is not connected.Press click button to connect.';
+  // tslint:disable-next-line: quotemark
+  public textDanger = "text-danger";
+  public allowButton = false;
+  public textBg = 'bg_primary';
+  public triggerType;
+  public inputStatus = '';
+  public address1 = ' ';
+
+
+  // tslint:disable-next-line: variable-name
   public main_Classes = {
-    
-    "font-size":"24px;",
-    "text-align":"center",
-    
+    'margin-bottom': '50px',
+    'font-size': '24px;',
+    'text-align': 'center',
+
+  };
+  onCreateServer(event) {
+    this.serverStatus = 'connected';
+    this.textDanger = 'text-success';
+    this.textBg = 'bg_success';
+    console.log(event);
+    this.triggerType = 'event trigger type is : ' + event.type;
+
+  }
+  updateSertverName(event: Event) {
+    // this.inputStatus=(<HTMLInputElement>event.target).value;
+    this.inputStatus = (event.target as HTMLInputElement).value;
+    console.log(event);
+
+
   }
 
   ngOnInit() {
